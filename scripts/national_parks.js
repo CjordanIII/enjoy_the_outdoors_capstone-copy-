f1a758bacbdcf86e2ceb4c66c3ef7ca205d71673
Clarence Jordan <clarencejordan087@gmail.com>
2024-05-20 11:46:27 -0500

    added: drop down templet logic

--- scripts/national_parks.js
+++ scripts/national_parks.js
@@ -21,10 +21,34 @@ import { nationalParksArray } from "./data/nationalParkData.js";
     */
 
 //log the locationsArray to the console (scripts/data/locationData.js)
-console.log(locationsArray);
+// locatoin or type radio btn logic
+const search = document.querySelector("#radioBtn");
+//TODO take in value depends on dropdown
+// const state = search.childNodes[1].childNodes[3].value;
+
+// search function
+function findByState(locationsArray) {
+  const byLocation = document.querySelector("#byLocation");
+  let display = true;
+  //   console.log(locationsArray);
+  //   console.log(parkTypesArray);
+  showDropdown(byLocation, display, locationsArray);
+}
+findByState(locationsArray);
+function showDropdown(child, display, arr) {
+  const dropdown = document.querySelector("#dropDown");
+  const clon = dropdown.content.cloneNode(true);
+  arr.forEach(function (item) {
+    const option = document.createElement("option");
+    option.textContent = item;
+    option.value = item;
+    clon.querySelector("select").appendChild(option);
+  });
+  document.body.appendChild(clon);
+}
 
 //log the parkTypesArray to the console (scripts/data/parkTypeData.js)
-console.log(parkTypesArray);
+// console.log(parkTypesArray);
 
-//   //log the nationalParksArray to the console (scripts/data/nationalParkData.js)
-console.log(nationalParksArray);
+//log the nationalParksArray to the console (scripts/data/nationalParkData.js)
+// console.log(nationalParksArray);
