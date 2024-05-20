c9ea987cf3fb451dc8bcb97a0ef0828e86e667a8
Clarence Jordan <clarencejordan087@gmail.com>
2024-05-20 11:18:33 -0500

    refacterd: js and added html to put data into

--- scripts/national_parks.js
+++ scripts/national_parks.js
@@ -1,22 +1,30 @@
-"use strict"
-
-window.onload = () => {
-
-    /*
+"use strict";
+/* 
+!Search by priority page
+    -requirements:
+        user can search for national parks inside nationalParkData.js file
+        two ways to search for national parks radio btn:
+            -By Location
+                -select dropdown (locationData file):
+                    -state
+                    -territory
+            -By park type
+ */
+// data files for national parks page
+import { locationsArray } from "./data/locationData.js";
+import { parkTypesArray } from "./data/parkTypeData.js";
+import { nationalParksArray } from "./data/nationalParkData.js";
+/*
         You can remove the following console.log() lines.
         They are here to verify that we have access to the data
         The data script files are located in the scripts/data directory
     */
 
-    //log the locationsArray to the console (scripts/data/locationData.js)
-    console.log(locationsArray)
+//log the locationsArray to the console (scripts/data/locationData.js)
+console.log(locationsArray);
 
-    //log the parkTypesArray to the console (scripts/data/parkTypeData.js)
-    console.log(parkTypesArray)
+//log the parkTypesArray to the console (scripts/data/parkTypeData.js)
+console.log(parkTypesArray);
 
-    //log the nationalParksArray to the console (scripts/data/nationalParkData.js)
-    console.log(nationalParksArray)
-
-
-
-}
+//   //log the nationalParksArray to the console (scripts/data/nationalParkData.js)
+console.log(nationalParksArray);
