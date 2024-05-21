82817d4a42f6bdaf8c492da108b74d92250c41db
Clarence Jordan <clarencejordan087@gmail.com>
2024-05-21 14:46:08 -0500

    fix moutain

--- scripts/mountains.js
+++ scripts/mountains.js
@@ -1,21 +1,34 @@
 "use strict";
 import { mountainsArray } from "./data/mountainData.js";
+import { showDropdown } from "./helpers/dropDownHelper.js";
+const dropDown = document.querySelector("#moutainDropDwon");
 /*
     You can remove the following console.log() lines.
     They are here to verify that we have access to the data
     The data script files are located in the scripts/data directory
 */
 const card = document.querySelector("#card");
+console.log(mountainsArray);
 
 mountainsArray.forEach((data) => {
-  card.innerHTML += `
-  <div class="card " style="width: 18rem">
-        <img src="${data.img}" class="card-img-top" alt="..." />
-        <div class="card-body">
-          <h5 class="card-title">${data.name}</h5>
-          <p class="card-text">
-           ${data.desc} </p>
-          </p>
-          <span class="card-text">Elevation:${data.elevation}</span>
-        </div>`;
+  const option = document.createElement("option");
+  option.innerText = data.name;
+  option.value = data.name;
+  dropDown.appendChild(option);
+});
+
+dropDown.addEventListener("change", (e) => {
+  const data = e.target.value;
+  console.log(data);
+  const result = mountainsArray.filter((moredata) => moredata.name === data);
+  card.innerHTML = `
+        <div class="card " style="width: 18rem">
+              <img src="${result[0].img}" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <h5 class="card-title">${result[0].name}</h5>
+                <p class="card-text">
+                 ${result[0].desc} </p>
+                </p>
+                <span class="card-text">Elevation:${result[0].elevation}</span>
+              </div>`;
 });
